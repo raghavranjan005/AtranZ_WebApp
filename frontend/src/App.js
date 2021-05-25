cb5356471bb208565571840069b9ca17a6046fc7
Raghav Ranjan <ranjan.1@iitj.ac.in>
2021-05-25 14:27:30 +0530

    live chat widget

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,6 +65,21 @@ function App(props) {
   // const takeToCart = () => {
   //   props.history.push("/signin?redirect=cart");
   // }
+
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60aca264a4114e480ad0b9ce/1f6h5kn3u';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
+
+
+
   return (
 
     <BrowserRouter>
