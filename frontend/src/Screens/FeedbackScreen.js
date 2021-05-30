8c03d86d7fd9c5077a1c148c1c51b649d8a9a91f
adityag5582 <kumar.92@iitj.ac.in>
2021-05-31 00:43:23 +0530

    changes

--- frontend/src/Screens/FeedbackScreen.js
+++ frontend/src/Screens/FeedbackScreen.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
-import smile from '../violet-smile.png'
 function FeedbackScreen() {
   
   return (
         <BrowserRouter>
         <div class="container">
-          <img src={smile}></img>
+          <img src="https://atranz.s3.ap-south-1.amazonaws.com/violet-smile.png"></img>
           <a href="https://forms.gle/5iMzG6Shstb8pm7S9" target="_blank">Click here to give valuable feedback      <i className="fa fa-angle-double-right"></i></a>
         </div>
         </BrowserRouter>
