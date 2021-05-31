d507d7c5775c951978b764d3d96341a703140a4b
Ubuntu <ubuntu@ip-172-31-2-145.ap-south-1.compute.internal>
2021-05-31 11:55:44 +0000

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/sh     
 sudo git pull origin main
-mkdir raghav
-echo tanuj1
+mkdir tanuj1
 sudo npm install
-echo tanuj2
+mkdir tanuj2
 cd frontend
-echo tanuj3
+mkdir tanuj3
 npm install
 sudo npm run build
-echo tanuj4
+mkdir tanuj4
 cd ..
 sudo systemctl restart nginx
 sudo pm2 restart all
