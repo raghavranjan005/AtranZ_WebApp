a982d8a111f141515c5e92a6f714a9387cae339b
Ubuntu <ubuntu@ip-172-31-2-145.ap-south-1.compute.internal>
2021-05-31 11:52:05 +0000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh     
 sudo git pull origin main
 mkdir raghav
+echo tanuj1
 sudo npm install
+echo tanuj2
 cd frontend
+echo tanuj3
 npm install
 sudo npm run build
+echo tanuj4
 cd ..
 sudo systemctl restart nginx
 sudo pm2 restart all
